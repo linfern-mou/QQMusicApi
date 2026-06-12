fc7fb66ecd931074f298e05d1814ad640462350d
Luren <68656403+luren-dc@users.noreply.github.com>
2026-06-12 14:36:29 +0800

    🧹 chore(release): bump version to 0.6.3

--- qqmusic_api/__init__.py
+++ qqmusic_api/__init__.py
@@ -18,7 +18,7 @@ from .core.exceptions import (
 from .core.versioning import Platform
 from .models.request import Credential
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 __all__ = [
     "ApiDataError",
